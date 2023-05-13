52fc6e6f07920fbc81e5303c17d1ee124622da42
torikushiii <torikushiii@gmail.com>
2023-05-14 03:50:31 +0800

    Initial Implementation

---
+++ services/google-script/README.md
@@ -0,0 +1,63 @@
+# Honkai: Star Rail Check-In Helper
+
+[Daily Check-In](https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html?act_id=e202303301540311)
+
+# Pre-requisites
+- Google Account
+
+# Installation
+1. Go to [Google Apps Script](https://script.google.com/home).
+2. Create a new project.
+    ![](https://i.imgur.com/y3FgPUV.png)
+3. You will be greeted with this screen, delete the contents of `myFunction` and click on "Untitled project" to rename it.
+    ![](https://i.imgur.com/4dRgXLe.png)
+4. Copy the contents of `index.js` and paste it into the editor.
+    ![](https://i.imgur.com/ZaA2oSX.png)
+5. Click on "Project settings".
+    ![](https://i.imgur.com/GMhLjEw.png)
+6. Scroll down and add the following to the "Script properties" section:
+   ```
+   COOKIE
+   DISCORD_WEBHOOK
+   ```
+    ![](https://i.imgur.com/BudKBiI.png)
+7. Add `COOKIE` with your cookie.
+8. Add `DISCORD_WEBHOOK` with your Discord webhook (optional).
+9. Click on "Save script properties".
+10. Click on "Editor" and then "Save Project" and "Run" to run it once.
+    ![](https://i.imgur.com/4B8jZea.png)
+    ![](https://i.imgur.com/SvNODZL.png)
+11. When first running the script, you will be prompted to authorize the script.
+    ![](https://i.imgur.com/igXjtkO.png)
+12. Click on "Review Permissions".
+13. Choose your Google account if prompted.
+14. You will be prompted to allow the script to access your Google account.
+    ![](https://i.imgur.com/n7gsH6o.png)
+15. Click on "Allow".
+16. After that when you run the script, the Execution log will show the output.
+    ![](https://i.imgur.com/KFGR003.png)
+
+# Getting your cookie
+1. Go to the Daily Check-In page [here](https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html?act_id=e202303301540311).
+2. Log in with your miHoYo account.
+3. Open the browser console (F12).
+4. Click on the "Console" tab.
+5. Type in `document.cookie` in the console and press Enter.
+6. Copy the output from the console.
+   ![](https://i.imgur.com/hFCL4yN.png)
+7. Paste it into the "Value" field for "COOKIE" in the "Script properties" section.
+8. Click on "Save script properties".
+
+# If you want to run it automatically every day
+1. Click on the "Triggers" icon.
+    ![](https://i.imgur.com/hAjfBhr.png)
+2. Click on "Add Trigger".
+    ![](https://i.imgur.com/WCVRpKA.png)
+3. Choose the following settings:
+    - Choose which function to run: `run`
+    - Choose which deployment should run: `Head`
+    - Select event source: `Time-driven`
+    - Select type of time based trigger: `Day timer`
+    - Select time of day: `Midnight to 1am`
+    ![](https://i.imgur.com/HSDge0k.png)
+4. Click on "Save".
---
+++ services/google-script/index.js
@@ -0,0 +1,239 @@
+// Your HSR Cookie should look like this
+// Ensure the name of the cookie is COOKIE
+// _MHYUUID=xxxx ; mi18nLang=en-us ; ltoken=xxxx ; ltuid=xxxx ; cookie_token=xxxx ; account_id=xxxx
+
+const scriptProperties = PropertiesService.getScriptProperties();
+
+const COOKIE = scriptProperties.getProperty("COOKIE");
+const DISCORD_WEBHOOK = scriptProperties.getProperty("DISCORD_WEBHOOK");
+
+const ACT_ID = "e202303301540311";
+const BASE_URL = "https://sg-public-api.hoyolab.com/event/luna/os";
+
+class Discord {
+  constructor(DISCORD_WEBHOOK) {
+    this.webhook = DISCORD_WEBHOOK;
+  }
+
+  async send(data = {}, logged = false) {
+    if (!this.webhook) {
+      return;
+    }
+
+    if (logged) {
+      const res = UrlFetchApp.fetch(this.webhook, {
+        method: "POST",
+        contentType: "application/json",
+        payload: JSON.stringify({
+          embeds: [
+            {
+              title: "Honkai: Star Rail Auto Check-in",
+              author: {
+                name: "Honkai: Star Rail",
+                icon_url: "https://i.imgur.com/o0hyhmw.png",
+              },
+              description: data.message,
+              color: 0xbb0bb5,
+              timestamp: new Date(),
+              footer: {
+                text: "Honkai: Star Rail Auto Check-in"
+              },
+            }
+          ],
+          username: "Honkai: Star Rail",
+          avatar_url: "https://i.imgur.com/o0hyhmw.png"
+        })
+      });
+
+      if (res.getResponseCode() !== 204) {
+        throw new Error(`Discord webhook error: ${res.getResponseCode()}`);
+      }
+
+      return true;
+    }
+
+    const embed = Discord.generateEmbed(data);
+    const res = UrlFetchApp.fetch(this.webhook, {
+      method: "POST",
+      contentType: "application/json",
+      payload: JSON.stringify({
+        embeds: [embed],
+        username: "Honkai: Star Rail",
+        avatar_url: "https://i.imgur.com/o0hyhmw.png"
+      })
+    });
+
+    if (res.getResponseCode() !== 204) {
+      throw new Error(`Discord webhook error: ${res.getResponseCode()}`);
+    }
+
+    return true;
+  }
+
+  static generateEmbed(data = {}) {
+    return {
+      title: "Honkai: Star Rail Auto Check-in",
+      author: {
+        name: "Honkai: Star Rail",
+        icon_url: "https://i.imgur.com/o0hyhmw.png",
+      },
+      description: `Today's reward: ${data.award.name} x${data.award.count}`
+        + `\nTotal signed: ${data.signed}`,
+      color: 0xbb0bb5,
+      timestamp: new Date(),
+      footer: {
+        text: "Honkai: Star Rail Auto Check-in"
+      },
+    };
+  }
+}
+
+class StarRail {
+  constructor() {
+    this.cookie = COOKIE;
+    this.baseUrl = BASE_URL;
+  }
+
+  static async sign() {
+    const options = {
+      method: "POST",
+      headers: {
+        "User-Agent": StarRail.userAgent,
+        Cookie: COOKIE
+      },
+      payload: {
+        act_id: ACT_ID
+      }
+    };
+
+    const res = UrlFetchApp.fetch(`${BASE_URL}/sign`, options);
+
+    if (res.getResponseCode() !== 200) {
+      throw new Error(`HTTP error: ${res.getResponseCode()}`);
+    }
+
+    return JSON.parse(res.getContentText());
+  }
+
+  async run() {
+    const cookie = this.cookie;
+    if (!cookie) {
+      throw new Error("cookie is required");
+    }
+    else if (typeof cookie !== "string") {
+      throw new Error("cookie must be a string");
+    }
+
+    const info = await StarRail.getInfo();
+    if (info.retcode !== 0 && info.message !== "OK") {
+      throw new Error(`API error: ${info.message}`);
+    }
+
+    const awards = await StarRail.awards();
+    if (awards.length === 0) {
+      throw new Error("There's no awards to claim (?)");
+    }
+
+    const data = {
+      today: info.data.today,
+      total: info.data.total_sign_day,
+      issigned: info.data.is_sign,
+      missed: info.data.sign_cnt_missed
+    };
+
+    const discord = new Discord(DISCORD_WEBHOOK);
+    if (data.issigned) {
+      await discord.send({ message: "You've already checked in today, Trailblazer~" }, true);
+      return {
+        message: "You've already checked in today, Trailblazer~"
+      };
+    }
+
+    const totalSigned = data.total;
+    const awardData = {
+      name: awards[totalSigned].name,
+      count: awards[totalSigned].cnt
+    };
+
+    const sign = await StarRail.sign();
+    if (sign.retcode !== 0 && sign.message !== "OK") {
+      throw new Error(`API error: ${sign.message}`);
+    }
+
+    console.log(`Signed in successfully! You have signed in for ${data.total} days!`);
+    console.log(`You have received ${awardData.count}x ${awardData.name}!`);
+
+    if (!DISCORD_WEBHOOK || typeof DISCORD_WEBHOOK !== "string") {
+      console.log("No Discord webhook provided, skipping...");
+      return true;
+    }
+
+    await discord.send({
+      signed: data.total,
+      award: awardData
+    });
+
+    return {
+      message: `Signed in successfully! You have signed in for ${data.total} days!`,
+    };
+  }
+
+  static async getInfo() {
+    const options = {
+      headers: {
+        "User-Agent": StarRail.userAgent,
+        Cookie: COOKIE
+      },
+      muteHttpExceptions: true,
+      method: "GET"
+    };
+
+    const res = UrlFetchApp.fetch(`${BASE_URL}/info?act_id=${ACT_ID}`, options);
+
+    if (res.getResponseCode() !== 200) {
+      throw new Error(`HTTP error: ${res.getResponseCode()}`);
+    }
+
+    return JSON.parse(res.getContentText());
+  }
+
+  static async awards() {
+    const options = {
+      headers: {
+        "User-Agent": StarRail.userAgent,
+        Cookie: COOKIE
+      },
+      muteHttpExceptions: true,
+      method: "GET"
+    };
+
+    const res = UrlFetchApp.fetch(`${BASE_URL}/home?act_id=${ACT_ID}`, options);
+
+    if (res.getResponseCode() !== 200) {
+      throw new Error(`HTTP error: ${res.getResponseCode()}`);
+    }
+
+    const body = JSON.parse(res.getContentText());
+
+    if (body.retcode !== 0 && body.message !== "OK") {
+      throw new Error(`API error: ${body.message}`);
+    }
+
+    return body.data.awards;
+  }
+
+  static get userAgent() {
+    return "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36";
+  }
+}
+
+function run() {
+  const starRail = new StarRail();
+  const run = starRail.run()
+    .then((res) => {
+      console.log(res.message);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
