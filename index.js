94501370c77e01c31d03279355b9842e4ed4984c
torikushiii <torikushiii@gmail.com>
2023-05-18 01:50:25 +0800

    Some code overhaul change
    - Custom logging
    - Better Discord message notify
    - Custom got module

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import got from "./lib/got.js";
 import { CronJob } from "cron";
 import { config } from "dotenv";
-import Discord from "./discord.js";
-import got, { HTTPError, TimeoutError } from "got";
+import logger from "./lib/winston.js";
+import DiscordModule from "./lib/discord.js";
 
 config();
 
@@ -9,182 +10,197 @@ const COOKIE = process.env.COOKIE;
 const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK;
 
 const ACT_ID = "e202303301540311";
-const BASE_URL = "https://sg-public-api.hoyolab.com/event/luna/os";
 
 class StarRail {
-    #cookie;
+	#cookie;
+	discordMessage = [];
 
-    constructor (cookie) {
-        if (typeof cookie !== "string" || typeof cookie === "undefined") {
-            throw new Error("cookie must be a string");
-        }
+	constructor (cookie) {
+		if (typeof cookie !== "string" || typeof cookie === "undefined") {
+			throw new Error("cookie must be a string");
+		}
 
-        this.#cookie = cookie;
-    }
+		this.#cookie = cookie;
+	}
 
-    static async sign (cookie) {
-        const res = await got({
-            method: "POST",
-            url: `${BASE_URL}/sign`,
-            responseType: "json",
-            headers: {
-                "User-Agent": StarRail.userAgent,
-                Cookie: cookie
-            },
-            json: {
-                act_id: ACT_ID
-            }
-        });
+	static async sign (cookie) {
+		const res = await got({
+			method: "POST",
+			url: "sign",
+			headers: {
+				Cookie: cookie
+			},
+			json: {
+				act_id: ACT_ID
+			}
+		});
 
-        if (res.statusCode !== 200) {
-            throw new Error(`HTTP error: ${res.statusCode}`);
-        }
+		if (res.statusCode !== 200) {
+			throw new Error(`[sign] HTTP Error: ${res.statusCode} ${res.statusMessage}`);
+		}
 
-        return res.body;
-    }
+		if (res.body.retcode !== 0 && res.body.message !== "OK") {
+			throw new Error(`[sign] API Error: ${res.body.message}`);
+		}
 
-    async run () {
-        const cookies = this.#parseCookies;
+		return res.body;
+	}
 
-        let counter = 0;
-        for (const cookie of cookies) {
-            counter++;
+	async run () {
+		const cookies = this.#parseCookies;
 
-            const info = await StarRail.getInfo(cookie);
-            if (info.retcode !== 0 && info.message !== "OK") {
-                throw new Error(`API error: ${info.message}`);            
-            }
+		let counter = 0;
+		for (const cookie of cookies) {
+			counter++;
 
-            const awards = await StarRail.awards(cookie);
-            if (awards.length === 0) {
-                throw new Error("There's no awards to claim (?)");
-            }
+			const [infoData, awardsData] = await Promise.allSettled([
+				StarRail.getInfo(cookie),
+				StarRail.awards(cookie)
+			]);
 
-            const data = {
-                today: info.data.today,
-                total: info.data.total_sign_day,
-                issigned: info.data.is_sign,
-                missed: info.data.sign_cnt_missed
-            };
+			if (infoData.status === "rejected") {
+				throw new Error(`API error: ${infoData.reason.message}`);
+			}
 
-            const discord = new Discord();
-            if (data.issigned) {
-                await discord.send({ message: `[Account ${counter}]: You've already checked in today, Trailblazer~` }, true);
-                console.log(`[Account ${counter}]: You've already checked in today, Trailblazer~`);
+			if (awardsData.status === "rejected") {
+				throw new Error(`API error: ${awardsData.reason.message}`);
+			}
 
-                continue;
-            }
+			const info = infoData.value;
+			const awards = awardsData.value;
 
-            const totalSigned = data.total;
-            const awardData = {
-                name: awards[totalSigned].name,
-                count: awards[totalSigned].cnt
-            };
+			if (awards.length === 0) {
+				logger.warn(`[Account ${counter}]: No awards found (?)`);
+				continue;
+			}
 
-            const sign = await StarRail.sign(cookie);
-            if (sign.retcode !== 0 && sign.message !== "OK") {
-                throw new Error(`API error: ${sign.message}`);
-            }
+			const data = {
+				today: info.data.today,
+				total: info.data.total_sign_day,
+				issigned: info.data.is_sign,
+				missed: info.data.sign_cnt_missed
+			};
 
-            console.log(`[Account ${counter}]: Signed in successfully! You have signed in for ${data.total} days!`);
-            console.log(`[Account ${counter}]: You have received ${awardData.count}x ${awardData.name}!`);
+			if (data.issigned) {
+				logger.warn(`[Account ${counter}]: You've already checked in today, Trailblazer~`);
+				this.discordMessage.push({
+					account: counter,
+					signed: data.total,
+					result: "You've already checked in today, Trailblazer~",
+					award: {
+						name: awards[data.total].name,
+						count: awards[data.total].cnt
+					}
+				});
+                
+				continue;
+			}
 
-            if (!DISCORD_WEBHOOK || typeof DISCORD_WEBHOOK !== "string") {
-                console.log("No Discord webhook provided, skipping...");
-                continue;
-            }
+			const totalSigned = data.total;
+			const awardData = {
+				name: awards[totalSigned].name,
+				count: awards[totalSigned].cnt
+			};
 
-            await discord.send({
-                signed: data.total,
-                award: awardData
-            });
-        }
-    }
+			const sign = await StarRail.sign(cookie);
+			if (sign.retcode !== 0 && sign.message !== "OK") {
+				throw new Error(`[Account ${counter}]: API error: ${sign.message}`);
+			}
 
-    static async getInfo (cookie) {
-        try {
-            const res = await got({
-                url: `${BASE_URL}/info`,
-                responseType: "json",
-                headers: {
-                    "User-Agent": StarRail.userAgent,
-                    Cookie: cookie
-                },
-                searchParams: {
-                    act_id: ACT_ID
-                }
-            });
+			logger.info(`[Account ${counter}]: Signed in successfully! You have signed in for ${data.total} days!`);
+			logger.info(`[Account ${counter}]: You have received ${awardData.count}x ${awardData.name}!`);
 
-            if (res.statusCode !== 200) {
-                throw new Error(`HTTP error: ${res.statusCode}`);
-            }
-    
-            return res.body;
-        }
-        catch (e) {
-            if (e instanceof HTTPError) {
-                throw new Error(`HTTP error: ${e.message}`);
-            }
-            else if (e instanceof TimeoutError) {
-                return StarRail.getInfo();
-            }
+			if (!DISCORD_WEBHOOK || typeof DISCORD_WEBHOOK !== "string") {
+				logger.info("No Discord webhook found, skipping Discord message");
+				continue;
+			}
 
-            throw e;
-        }
-    }
+			this.discordMessage.push({
+				account: counter,
+				signed: data.total,
+				result: "OK",
+				award: awardData
+			});
+		}
 
-    static async awards (cookie) {
-        const res = await got({
-            url: `${BASE_URL}/home`,
-            responseType: "json",
-            headers: {
-                "User-Agent": StarRail.userAgent,
-                Cookie: cookie
-            },
-            searchParams: {
-                act_id: ACT_ID
-            }
-        });
+		if (this.discordMessage.length === 0) {
+			return;
+		}
 
-        if (res.statusCode !== 200) {
-            throw new Error(`HTTP error: ${res.statusCode}`);
-        }
+		const Discord = new DiscordModule();
+		await Discord.send(this.discordMessage);
 
-        if (res.body.retcode !== 0 && res.body.message !== "OK") {
-            throw new Error(`API error: ${res.body.message}`);
-        }
+		return;
+	}
 
-        return res.body.data.awards;
-    }
+	static async getInfo (cookie) {
+		const res = await got({
+			url: "info",
+			headers: {
+				Cookie: cookie
+			},
+			searchParams: {
+				act_id: ACT_ID
+			}
+		});
 
-    get #parseCookies () {
-        return this.#cookie.split("#");
-    }
+		if (res.statusCode !== 200) {
+			throw new Error(`[getInfo] HTTP error: ${res.statusCode}`);
+		}
 
-    get userAgent () {
-        return "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36";
-    }
-};
+		if (res.body.retcode !== 0 && res.body.message !== "OK") {
+			throw new Error(`[getInfo] API error: ${res.body.message}`);
+		}
+
+		return res.body;
+	}
+
+	static async awards (cookie) {
+		const res = await got({
+			url: "home",
+			headers: {
+				Cookie: cookie
+			},
+			searchParams: {
+				act_id: ACT_ID
+			}
+		});
+
+		if (res.statusCode !== 200) {
+			throw new Error(`[awards] HTTP error: ${res.statusCode}`);
+		}
+
+		if (res.body.retcode !== 0 && res.body.message !== "OK") {
+			throw new Error(`[awards] API error: ${res.body.message}`);
+		}
+
+		return res.body.data.awards;
+	}
+
+	get #parseCookies () {
+		return this.#cookie.split("#");
+	}
+}
 
 export default StarRail;
 
 if (process.argv.includes("--sign")) {
-    const starRail = new StarRail(COOKIE);
-    await starRail.run();
+	const starRail = new StarRail(COOKIE);
+	await starRail.run();
 
-    process.exit(0);
+	process.exit(0);
 }
 else {
-    /**
+	/**
      * Adjust the time to your liking.
      * Or when does the daily reset happen in your timezone.
      */
-    const job = new CronJob("0 0 0 * * *", async () => {
-        const starRail = new StarRail(COOKIE);
-        await starRail.run();
-    });
+	const job = new CronJob("0 0 0 * * *", async () => {
+		const starRail = new StarRail(COOKIE);
+		await starRail.run();
+	});
 
-    job.start();
+	job.start();
 
-    console.log("Cron job started");
+	logger.info("Cron job started");
 }
