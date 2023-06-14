9326b1acd3145f889100839ffd9d6639ad6b677d
torikushiii <torikushiii@gmail.com>
2023-06-15 03:41:59 +0800

    fixed cron path

--- crons/index.mjs
+++ crons/index.mjs
@@ -1,6 +1,6 @@
 import { definitions as Stamina } from "./stamina/index.mjs";
 import { definitions as CheckIn } from "./check-in/index.mjs";
-import { definitions as Expedition } from "./expidition/index.mjs";
+import { definitions as Expedition } from "./expedition/index.mjs";
 
 export const definitions = [
 	Stamina,
